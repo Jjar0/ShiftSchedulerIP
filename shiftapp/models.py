9647e334d53918a7064881543dae5f88382ff3f3
Jjar0 <peterhayward1707@gmail.com>
2025-05-07 23:44:07 +0100

    models updated

--- shiftapp/models.py
+++ shiftapp/models.py
@@ -5,4 +5,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # 'admin' or 'employee'
+    role = db.Column(db.String(50), nullable=False)  # admin or employee
