4f3c55cd525cc471c5868323a8e1a14a66c91ce7
Jjar0 <peterhayward1707@gmail.com>
2025-05-07 23:43:27 +0100

    built basic login with form

--- shiftapp/__init__.py
+++ shiftapp/__init__.py
@@ -3,11 +3,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yoursecretkey'
+app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shifts.db'
 
 db = SQLAlchemy(app)
+
 loginManager = LoginManager(app)
 loginManager.login_view = 'login'
+
+from shiftapp.models import User
+
+@loginManager.user_loader
+def loadUser(userId):
+    return User.query.get(int(userId))
 
 from shiftapp import routes
--- shiftapp/routes.py
+++ shiftapp/routes.py
@@ -1,10 +1,24 @@
-from flask import render_template, redirect, url_for
-from shiftapp import app
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required
+from shiftapp import app, db
+from shiftapp.models import User
+from shiftapp.forms import LoginForm
 
 @app.route('/')
 def home():
     return redirect(url_for('login'))
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:  # Replace with hash check later
+            login_user(user)
+            if user.role == 'admin':
+                return redirect(url_for('adminDashboard'))
+            else:
+                return redirect(url_for('employeeDashboard'))
+        else:
+            flash('Invalid credentials')
+    return render_template('login.html', form=form)
