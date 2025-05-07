96f9ea7d6d171bb071578a094ce520a12ecfef8d
Jjar0 <peterhayward1707@gmail.com>
2025-05-07 22:55:14 +0100

    added routing

--- shiftapp/routes.py
+++ shiftapp/routes.py
@@ -1,1 +1,10 @@
+from flask import render_template, redirect, url_for
+from shiftapp import app
 
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
